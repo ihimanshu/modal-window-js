d37dd5fa9c5d0f4b1a9e1f64162363ac3cc718ee
Himanshu Sareen <23.himanshusareen@gmail.com>
2021-02-23 18:32:00 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,28 @@
+'use strict';
+
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnCloseModal = document.querySelector('.close-modal');
+const btnsShowModal = document.querySelectorAll('.show-modal');
+
+const closeModal = function () {
+  overlay.classList.add('hidden');
+  modal.classList.add('hidden');
+};
+
+const openModal = function () {
+  overlay.classList.remove('hidden');
+  modal.classList.remove('hidden');
+};
+
+for (let i = 0; i < btnsShowModal.length; i++)
+  btnsShowModal[i].addEventListener('click', openModal);
+
+btnCloseModal.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
+
+document.addEventListener('keydown', function (e) {
+  if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
+    closeModal();
+  }
+});
